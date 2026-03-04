45edff836830a6ab3a87feda383a8195cc242cad
medsnok7 <mohamedc@RPTL-70.kpit.com>
2026-03-04 02:53:16 +0100

    [MoCh/TID-001] automate docker image pulling

--- .devcontainer/pre_spawn.sh
+++ .devcontainer/pre_spawn.sh
@@ -3,4 +3,4 @@ echo "removing all other containers to stop their processes"
 echo "stopping $1"
 dsi $1
 echo "will try to pull the image $1:$2"
-docker login -u $3 -p dckr_pat_76ra0RSNHTMYLBHgNBae_lZAUvQ && docker pull $3/$1:$2 && docker logout
+docker login -u $3 -p $4 && docker pull $3/$1:$2 && docker logout
